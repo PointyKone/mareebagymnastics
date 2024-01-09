e5301e404fdf5444f827a5412fbbe296df24c6cf
Kian Saeidnia <ksaeidnia06@gmail.com>
2024-01-09 18:29:15 +1000

    init plus navbar

---
+++ src/app/_components/dropdown.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import { NavItem } from "./navbar"
+
+
+export function Dropdown({ name, items }: { name: string, items: { name: string, href: string }[] }) {
+    const [open, setOpen] = useState<Boolean>(false)
+
+    return (
+        <div className="my-auto relative">
+            <button onMouseLeave={() => setOpen(false)} onMouseOver={() => setOpen(true)} className="pl-8 font-extralight flex" key={name}>{name}<span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 pl-2 h-full my-auto">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+            </svg></span></button>
+            <div onMouseLeave={() => setOpen(false)} onMouseOver={() => setOpen(true)} className={`${open ? "flex opacity-100 z-10" : "invisible opacity-0"} absolute flex-col flex justify-end text-center bg-white p-8 shadow-md rounded-md left-[-16px] top-12 transition-all ease-in-out duration-150 `}>
+                {items.map(item => {
+                    {
+                        if (item.name === "") {
+                            return
+                        }
+                    }
+                    return (
+                        <a className="py-2 font-extralight" href={item.href}>{item.name}</a>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+export type NavItem = {
+    name?: string
+    items: {
+        name: string
+        href: string
+    }[]
+}
+
+import Link from "next/link"
+import { Dropdown } from "./dropdown"
+
+export function Navbar() {
+
+    const nav: NavItem[] = [
+        { items: [{name: "Home", href: "/"}]},
+        { name: "Club", items: [{name: "Team", href: "/club/team"}, { name: "Sponsers", href: "/club/sponsers"}]},
+        { items: [{name: "Classes", href: "/classes"}]},
+        { items: [{name: "Book Now", href: "/book"}]},
+        { items: [{name: "Holidays", href: "/holidays"}]},
+        { name: "Competitions", items: [{name: "Nomination Links", href: "/competitions/nominations"}, { name: "2022 Competition Results", href: "/competitions/results/2022"}, { name: "2021 Competition Results", href: "/competitions/results/2021"},]},
+        { name: "News", items: [{name: "Club Calendar", href: "/calendar"}, {name: "", href: "/"}]},
+        { items: [{name: "Contact", href: "/contact"}]},
+        { items: [{name: "FAQ", href: "/faq"}]},
+        { items: [{name: "Shop", href: "/shop"}]},
+    ]
+    return (
+        <nav className="h-20 w-full bg-[#92d0da] px-36 py-2 flex justify-between">
+            <img src="/logo/transparent-white.png" alt="" className="" />
+            <div className="text-center flex justify-end align-middle">
+
+                {nav.map(i => {
+                    if (i.items.length === 1) return <a className="pl-8 my-auto font-extralight" href={i.items[0]?.href}>{i.items[0]?.name}</a>
+        
+                    return (
+                        i.name ? <Dropdown name={i.name} items={i.items} /> : ""
+                    )
+
+                })}
+            </div>
+        </nav>
+    )
+}
